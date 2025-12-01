dab05dd49ab7ff4b7c3fd2bbf056988243b53b46
IlyaVafin <rthrthrer5gh@gmail.com>
2025-12-01 22:52:17 +0300

    feat: #6 finish quntities clients block

--- src/modules/quantity-clients/QuantityClients.tsx
+++ src/modules/quantity-clients/QuantityClients.tsx
@@ -1,13 +1,24 @@
-import TitleBlock from "@/src/components/title-block/title-block";
-import { Swiper, SwiperSlide } from "swiper/react";
+"use client"
+import TitleBlock from "@/src/components/title-block/title-block"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { clientsLogo } from "./const/clients-logo"
+import Image from "next/image"
 
 export default function QuantityClients() {
-  return (
-    <section>
-      <TitleBlock>500+ клиентов уже с нами</TitleBlock>
-      <Swiper>
-        <SwiperSlide></SwiperSlide>
-      </Swiper>
-    </section>
-  )
+	return (
+		<section className="mt-[75px]">
+			<TitleBlock>500+ клиентов уже с нами</TitleBlock>
+			<Swiper className="mt-10" breakpoints={{1024: {
+        slidesPerView: 5,
+      }, 1280: {
+        slidesPerView: 6,
+      }}} slidesPerView={3} spaceBetween={20}>
+				{clientsLogo.map(logo => (
+					<SwiperSlide key={logo.src}>
+						<Image className="h-20" src={logo.src} width={220} height={80} alt='' />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</section>
+	)
 }
---
+++ src/modules/quantity-clients/const/clients-logo.ts
@@ -0,0 +1,23 @@
+export const clientsLogo = [
+  {
+    src: '/36-6-logo.svg',
+  },
+  {
+    src: 'ho.svg'
+  },
+  {
+    src: 'aircraft.svg'
+  },
+  {
+    src: 'pokupochka.svg'
+  },
+  {
+    src: '/ortk.svg'
+  },
+  {
+    src: '/alenka.svg'
+  },
+  {
+    src: 'red.svg'
+  }
+]
