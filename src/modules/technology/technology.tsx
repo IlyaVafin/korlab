d2d330d7609e182ec7bc45e5f9cd5cea086963ce
IlyaVafin <rthrthrer5gh@gmail.com>
2025-12-02 11:49:04 +0300

    feat: #8 finish technology block

--- src/modules/technology/technology.tsx
+++ src/modules/technology/technology.tsx
@@ -1,24 +1,28 @@
 import Button from "@/src/components/button/button"
 import OrangeCard from "@/src/components/orange-card/orange-card"
+import Typography from "@/src/components/typography/typography"
 import Image from "next/image"
 
 export default function Technology() {
 	return (
-		<section>
-			<div className=''>
-				<Image
-					width={793}
-					height={308}
-					src='/technology.png'
-					alt='technology-image'
-				/>
-				<OrangeCard>
-					<p className="text-white">
-						Прокачайте свой интернет с высокоскоростными технологиями mrnet. Наш
-						мультироутер суммирует несколько каналов связи в один (технология
-						бондинг), значительно повышая устойчивость и скорость соединения!
-					</p>
-					<Button variant='aqua'>о технологии</Button>
+		<section className='mt-10 mb-10'>
+			<div className='flex flex-col items-center lg:flex-row lg:gap-3 lg:h-[308px] lg:items-start'>
+				<div className='relative w-[793px] h-[220px] max-w-full sm:h-[250px] md:h-[280px] lg:h-[308px]'>
+					<Image fill src='/technology.png' alt='technology-image' />
+				</div>
+				<OrangeCard className='max-w-[793px] lg:max-w-[563px] mt-2.5 mb-2.5 pb-[19px] h-full lg:mt-0 '>
+					<Typography
+						variant='description'
+						className='mb-[21px] xl:text-[24px] leading-[30px]'
+					>
+						Прокачайте свой интернет с высокоскоростными технологиями{" "}
+						<span className='font-medium'>mrnet</span>. Наш мультироутер
+						суммирует несколько каналов связи в один (технология бондинг),
+						значительно повышая устойчивость и скорость соединения!
+					</Typography>
+					<Button className='mb-2' variant='aqua'>
+						о технологии
+					</Button>
 				</OrangeCard>
 			</div>
 		</section>
