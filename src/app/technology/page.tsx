d82cdac0ee2bdc4046126dcc2edcf6f1f2180231
IlyaVafin <rthrthrer5gh@gmail.com>
2025-12-06 18:38:36 +0300

    feat: #27 add hero block on technology page

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,3 +1,10 @@
+import Container from "@/src/components/container/container";
+import Hero from "@/src/modules/technolohy-page/hero/hero";
+
 export default function Technology() {
-  
+  return (
+    <Container>
+      <Hero/>
+    </Container>
+  )
 }
--- src/modules/technolohy-page/hero/hero.tsx
+++ src/modules/technolohy-page/hero/hero.tsx
@@ -5,23 +5,26 @@ import Image from "next/image"
 
 export default function Hero() {
 	return (
-		<section>
-			<div className=''>
+		<section className="mt-[50px]">
+			<div className='flex flex-col gap-2.5 lg:flex-row'>
 				<Image
 					src={"/hero-technology.png"}
 					width={0}
 					height={0}
+          className="w-full h-auto"
 					sizes='100vw'
 					alt='hero-image'
 				/>
 				<Card variant='aqua'>
-					<Typography variant='description'>
+					<Typography className="mb-6 lg:text-[26px] lg:leading-[35px]" variant='description'>
 						РЕШЕНИЕ mrnet С ТЕХНОЛОГИЕЙ BONDING ОБЪЕДИНЯЕТ ВСЕ ДОСТУПНЫЕ КАНАЛЫ
 						СВЯЗИ В ЕДИНЫЙ ВИРТУАЛЬНЫЙ КАНАЛ, СУММИРУЕТ СКОРОСТЬ ПЕРЕДАЧИ ДАННЫХ
 						И ПОВЫШАЕТ НАДЕЖНОСТЬ СОЕДИНЕНИЯ
 					</Typography>
-					<Button variant='aqua'>оставить заявку</Button>
-					<Typography variant="description">
+					<div className="max-w-[334px]">
+            <Button variant='aqua'>оставить заявку</Button>
+          </div>
+					<Typography className="mt-[50px]" variant="description">
 						Оставьте заявку и с вами свяжется менеджер и договорится о запуске
 						пилота. Без необходимости отключения от текущего провайдера и
 						заключения договора. Все цены рассчитываются индивидуально под
